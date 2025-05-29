3433b7eb99d849a15ff0eaad9d387d2116bc60c6
F <948040237@qq.com>
2025-05-30 07:18:21 +0800

    feat 优化配置

--- script/zw_build.bat
+++ script/zw_build.bat
@@ -1,18 +1,18 @@
 @echo on
 
-set "targetDirectory=D:\go\release\zw\devtool"
+set "targetDirectory=C:\work\frog\tool_zw\devtool"
 if not exist "%targetDirectory%" (
     echo dir：%targetDirectory% not exist,creating...
     mkdir "%targetDirectory%"
 )
 
-set "targetDirectory1=D:\go\release\zw\goservice\build"
+set "targetDirectory1=C:\work\frog\tool_zw\goservice\build"
 if not exist "%targetDirectory1%" (
     echo dir：%targetDirectory1% not exist,creating...
     mkdir "%targetDirectory1%"
 )
 
-set "targetDirectory2=D:\go\release\zwPub\goservice\build"
+set "targetDirectory2=C:\work\frog\tool_zwPub\goservice\build"
 if not exist "%targetDirectory2%" (
     echo dir：%targetDirectory2% not exist,creating...
     mkdir "%targetDirectory2%"
@@ -24,28 +24,28 @@ if not exist "%targetDirectory3%" (
     mkdir "%targetDirectory3%"
 )
 
-copy D:\job\cache_manager_api\build\zw.exe D:\go\release\zw\goservice\build\zw.exe /Y
-xcopy D:\job\cache_manager_api\config\zw D:\go\release\zw\goservice\config\zw /E /Y /I
-xcopy D:\job\cache_manager_api\script\zw_start.bat D:\go\release\zw\ /y
-xcopy D:\job\cache_manager_api\go.mod D:\go\release\zw\goservice /y
-xcopy D:\job\cache_manager_api\internal\pkg\p_js D:\go\release\zw\goservice\internal\pkg\p_js /E /Y /I
-xcopy D:\job\cache_manager_api\internal\pkg\p_node D:\go\release\zw\goservice\internal\pkg\p_node /E /Y /I
-xcopy D:\job\cache_manager_web\public\favicon.ico D:\go\release\zw\devtool /y
-xcopy D:\job\cache_manager_web\dist D:\go\release\zw\devtool\dist /E /Y /I
+copy C:\work\frog\cache_manager_api\build\zw.exe C:\work\frog\tool_zw\goservice\build\zw.exe /Y
+xcopy C:\work\frog\cache_manager_api\config\zw C:\work\frog\tool_zw\goservice\config\zw /E /Y /I
+xcopy C:\work\frog\cache_manager_api\script\zw_start.bat C:\work\frog\tool_zw\ /y
+xcopy C:\work\frog\cache_manager_api\go.mod C:\work\frog\tool_zw\goservice /y
+xcopy C:\work\frog\cache_manager_api\internal\pkg\p_js C:\work\frog\tool_zw\goservice\internal\pkg\p_js /E /Y /I
+xcopy C:\work\frog\cache_manager_api\internal\pkg\p_node C:\work\frog\tool_zw\goservice\internal\pkg\p_node /E /Y /I
+xcopy C:\work\frog\cache_manager_web\public\favicon.ico C:\work\frog\tool_zw\devtool /y
+xcopy C:\work\frog\cache_manager_web\dist C:\work\frog\tool_zw\devtool\dist /E /Y /I
 
-copy D:\job\cache_manager_api\build\zwPub.exe D:\go\release\zwPub\goservice\build\zw.exe /Y
-xcopy D:\job\cache_manager_api\config D:\go\release\zwPub\goservice\config\ /E /Y /I
-xcopy D:\job\cache_manager_api\internal\pkg\p_js D:\go\release\zwPub\goservice\internal\pkg\p_js /E /Y /I
-xcopy D:\job\cache_manager_api\internal\pkg\p_node D:\go\release\zwPub\goservice\internal\pkg\p_node /E /Y /I
-xcopy D:\job\cache_manager_api\script\zw_start.bat D:\go\release\zwPub\ /y
-xcopy D:\job\cache_manager_api\go.mod D:\go\release\zwPub\goservice\ /y
-xcopy D:\job\cache_manager_web\public\favicon.ico D:\go\release\zwPub\devtool\ /y
-xcopy D:\job\cache_manager_web\dist D:\go\release\zwPub\devtool\dist\ /E /Y /I
-if exist "D:\go\release\zwPub\goservice\playwright.RunLock" (
-    del /f /q "D:\go\release\zwPub\goservice\playwright.RunLock"
+copy C:\work\frog\cache_manager_api\build\zwPub.exe C:\work\frog\tool_zwPub\goservice\build\zw.exe /Y
+xcopy C:\work\frog\cache_manager_api\config C:\work\frog\tool_zwPub\goservice\config\ /E /Y /I
+xcopy C:\work\frog\cache_manager_api\internal\pkg\p_js C:\work\frog\tool_zwPub\goservice\internal\pkg\p_js /E /Y /I
+xcopy C:\work\frog\cache_manager_api\internal\pkg\p_node C:\work\frog\tool_zwPub\goservice\internal\pkg\p_node /E /Y /I
+xcopy C:\work\frog\cache_manager_api\script\zw_start.bat C:\work\frog\tool_zwPub\ /y
+xcopy C:\work\frog\cache_manager_api\go.mod C:\work\frog\tool_zwPub\goservice\ /y
+xcopy C:\work\frog\cache_manager_web\public\favicon.ico C:\work\frog\tool_zwPub\devtool\ /y
+xcopy C:\work\frog\cache_manager_web\dist C:\work\frog\tool_zwPub\devtool\dist\ /E /Y /I
+if exist "C:\work\frog\tool_zwPub\goservice\playwright.RunLock" (
+    del /f /q "C:\work\frog\tool_zwPub\goservice\playwright.RunLock"
 )
-if exist "D:\go\release\zwPub.zip" (
-    del /f /q "D:\go\release\zwPub.zip"
+if exist "C:\work\frog\tool_zwPub.zip" (
+    del /f /q "C:\work\frog\tool_zwPub.zip"
 )
 
-"C:\Program Files\WinRAR\winrar.exe" a -afzip -r -ep1 D:\go\release\zwPub.zip D D:\go\release\zwPub
+"C:\Program Files\WinRAR\winrar.exe" a -afzip -r -ep1 C:\work\frog\tool_zwPub.zip D C:\work\frog\tool_zwPub
